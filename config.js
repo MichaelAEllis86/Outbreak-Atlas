2a57be1f5a57ea95d2e4f69a16e5e0c5a3e248a7
Michael Ellis <michaelaellis86@gmail.com>
2025-10-20 16:28:23 -0400

    Add production config and .env setup for Render

--- config.js
+++ config.js
@@ -1,21 +1,24 @@
-/** Shared config for application; can be req'd many places. */
+/** Shared config for application; can be required many places. */
 
-require('dotenv').config();
-const SECRET_KEY = process.env.SECRET_KEY || 'development-secret-key';
+require("dotenv").config();
 
+const SECRET_KEY = process.env.SECRET_KEY || "development-secret-key";
 const PORT = +process.env.PORT || 3000;
-
 const BCRYPT_WORK_FACTOR = 12;
 
-const DB_URI =
-  process.env.NODE_ENV === 'test'
-    ? "postgres://mooks2022:mookster21@localhost/outbreak_atlas_test"
-    : "postgres://mooks2022:mookster21@localhost/outbreak_atlas";
-    
-        
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+  DB_URI = process.env.TEST_DATABASE_URL;
+} else if (process.env.NODE_ENV === "production") {
+  DB_URI = process.env.DATABASE_URL;
+} else {
+  DB_URI = process.env.DEV_DATABASE_URL;
+}
+
 module.exports = {
   BCRYPT_WORK_FACTOR,
   SECRET_KEY,
   PORT,
-  DB_URI
+  DB_URI,
 };
