f6c4222f0a46238c09730b18b37b5198e0a56068
Michael Ellis <michaelaellis86@gmail.com>
2025-10-22 18:06:25 -0400

    log for env var

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,8 @@ import AccountPage from "./AccountPage";
 
 export default function App() {
 
+  console.log('🔍 API Base URL →', import.meta.env.VITE_API_BASE_URL);
+
   return (
     <BrowserRouter>
       <AuthProvider>
